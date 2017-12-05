ea882b2ec73954a89b43d58e784fa157964a6565
sudshek <singhsudhanshu204@gmail.com>
2017-12-05 18:50:23 +0530

    Switched to Mlab Database

--- app.js
+++ app.js
@@ -5,21 +5,21 @@ mongoose.Promise = global.Promise;
 var BodyParser = require('body-parser');
 app.use(cors());
 var SignupApi = require('./routes/SignupApi.js')
-app.use(BodyParser.urlencoded({extended : true}));
+app.use(BodyParser.urlencoded({ extended: true }));
 app.use(BodyParser.json());
 var port = 8080;
 
-app.use('/',SignupApi);
+app.use('/', SignupApi);
 
-var uri = 'mongodb://localhost/example';
-mongoose.connect(uri, function(err){
-    if(err)
-    {throw err;}});
-   
+var uri = 'mongodb://keshav:keshav@ds119355.mlab.com:19355/exchanger';
+mongoose.connect(uri, function (err) {
+    if (err) { throw err; }
+});
+
 console.log('hi');
 app.listen(port, () => {
     console.log("Server listening on port " + port);
-   });
+});
 
 
 
